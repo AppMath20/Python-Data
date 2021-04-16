5d174121222b254088061437c24bdc69771bbfb5
AppMath20 <56472870+AppMath20@users.noreply.github.com>
2021-04-16 13:15:43 +0300

    Update test_public.py

--- Test/test_public.py
+++ Test/test_public.py
@@ -1,9 +1,10 @@
 import sys
 import unittest
 from Resource.directory_reader import DirReader
-
+import test 
 
 class TestF(unittest.TestCase):
     def test_DirReader(self):
         for file in DirReader("Test"):
             self.assertEqual(file, "Test" + "\\test.txt")
+
